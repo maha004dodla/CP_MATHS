42ee4e945b5aca6c61857359bb95b9c31f3596c3
maha <97425755+maha004dodla@users.noreply.github.com>
2024-01-21 22:07:26 +0530

    nCr.cpp

---
+++ nCr.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<long long>>pascal;
+long long mod=1e9;
+void ans(long long n)
+{
+    pascal.push_back({1,1});
+    long long size=2;
+    for(long long i=1; i<n; i++)
+    {
+        
+        vector<long long>l;
+        l.push_back(1);
+        for(long long j=1; j<size; j++)
+        {
+            long long val=pascal[i-1][j-1]%mod+pascal[i-1][j]%mod;
+            l.push_back(val%mod);
+        }
+        l.push_back(1);
+        size=l.size();
+        pascal.push_back(l);
+    }
+}
+int main()
+{
+    long long t,n;
+    cin>>t;
+    ans(1000);
+    while(t--)
+    {
+        long long i,j;
+        cin>>n;
+        for(i=0; i<=n; i++)
+        {
+            cout<<pascal[n-1][i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
